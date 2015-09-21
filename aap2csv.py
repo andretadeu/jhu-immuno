b8fa911a7f3cb8b55d7f92fb8587b7275bd02135
hfoffani <hfoffani@gmail.com>
2015-09-21 17:10:11 -0400

    Amino acid properties cleanup and converter to csv

---
+++ aap2csv.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# title + citation + 20 + blank == 23
+
+d = {}
+
+currname = ""
+with open("amino_acid_prop_cleaned.txt") as fi:
+    for i, x in enumerate(fi.readlines()):
+        if i % 23 == 0:
+            currname = '_' + x.replace('\n','').replace(' ','_').replace('(','').replace(')','').replace('.','')
+            d[currname] = []
+        if i % 23 == 1:
+            continue
+        if i % 23 == 22:
+            continue
+        else:
+            pass
+            d[currname].append(float(x.split()[0]) )
+
+#print d.keys()
+#print len(d)
+
+letters=[ 'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I',
+'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V' ]
+
+lkeys = list(d.keys())
+lkeys.sort()
+
+with open('amino_acid_prop_cleaned.csv', 'w') as fo:
+    fo.write('letter;');
+    for k in lkeys:
+        fo.write(k)
+        fo.write(';')
+    fo.write('\n')
+    for i in range(20):
+        fo.write(letters[i])
+        fo.write(';')
+        for k in lkeys:
+            fo.write(str(d[k][i]))
+            fo.write(';')
+        fo.write('\n')
+
+
+
+
+
