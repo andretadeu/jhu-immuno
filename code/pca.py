047186fefdc79d28505cc7b39b2d73ebcc67bc8a
andretadeu <andre.tadeu.de.carvalho@gmail.com>
2015-09-22 11:41:42 -0300

    Add .gitignore entry

--- code/pca.py
+++ code/pca.py
@@ -15,26 +15,28 @@ from sklearn.preprocessing import LabelEncoder
 # The iris dataset
 train    = pd.read_csv('~/sandbox/jhu-immuno/data/aaProp20x544.csv')
 
-labelCats = ['T1_V11','T1_V12','T1_V15','T1_V16','T1_V17','T1_V4','T1_V5','T1_V6','T1_V7','T1_V8','T1_V9','T2_V11','T2_V12','T2_V13','T2_V3','T2_V5']
-trainLabel = train[labelCats].copy()
-for label in labelCats:  
-    lbl = LabelEncoder()
-    trainLabel[label] = lbl.fit_transform(trainLabel[label])
-    
-train = pd.get_dummies(train)
+train.head()
 
-trainAll = pd.concat([train,trainLabel],axis=1)
+#labelCats = ['T1_V11','T1_V12','T1_V15','T1_V16','T1_V17','T1_V4','T1_V5','T1_V6','T1_V7','T1_V8','T1_V9','T2_V11','T2_V12','T2_V13','T2_V3','T2_V5']
+#trainLabel = train[labelCats].copy()
+#for label in labelCats:  
+#    lbl = LabelEncoder()
+#    trainLabel[label] = lbl.fit_transform(trainLabel[label])
+    
+#train = pd.get_dummies(train)
+
+#trainAll = pd.concat([train,trainLabel],axis=1)
 
 from sklearn.decomposition import PCA
 pca = PCA(n_components=100)
 
-y   = train.Hazard   
+#y   = train.Hazard   
 
-X = trainAll.drop('Hazard',axis=1)
+#X = trainAll.drop('Hazard',axis=1)
 pca.fit(X)
 
 print(pca.explained_variance_ratio_)
 
-X_new = pca.transform(X) 
-v0 = pca.transform(pca.components_[0])
-v0 /= v0[-1]
+#X_new = pca.transform(X) 
+#v0 = pca.transform(pca.components_[0])
+#v0 /= v0[-1]
