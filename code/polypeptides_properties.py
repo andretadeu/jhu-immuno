fa537ef4f968426d5bfdcad8829a2c371cbd5c83
hfoffani <hfoffani@gmail.com>
2015-09-23 11:06:41 -0400

    the polypeptides properties file

---
+++ code/polypeptides_properties.py
@@ -0,0 +1,100 @@
+
+# less 1394
+
+import sys
+
+
+# outputfile  = '../data/peptide_9_props.csv'
+# propvalfile = '../data/aaProp20x544.csv'
+outputfile  = '../data/polypeptide_9_props_norm.csv'
+propvalfile = '../data/aaProp20x544-norm.csv'
+
+peps = {}
+letmap = {
+'ALA': 'A',
+'ARG': 'R',
+'ASN': 'N',
+'ASP': 'D',
+'CYS': 'C',
+'GLN': 'Q',
+'GLU': 'E',
+'GLY': 'G',
+'HIS': 'H',
+'ILE': 'I',
+'LEU': 'L',
+'LYS': 'K',
+'MET': 'M',
+'PHE': 'F',
+'PRO': 'P',
+'SER': 'S',
+'THR': 'T',
+'TRP': 'W',
+'TYR': 'Y',
+'VAL': 'V'
+}
+
+props = {
+'X': [0.0] * 544
+}
+
+# infile ='../input/journal.pcbi.1003266.csv'
+infile = "../data/polypeptides.txt"
+
+with open(infile) as fjour:
+    for i, l in enumerate(fjour.readlines()):
+        ls = l[0:-1]
+        if len(ls) != 9:
+            continue
+        peps[ls] = None
+
+# print peps
+print len(peps)
+# sys.exit(0)
+
+def fil(x):
+    if x == 'NA':
+        return 0.0
+    else:
+        return float(x)
+
+        
+with open(propvalfile) as fprop:
+    for i, l in enumerate(fprop.readlines()):
+        if i == 0:
+            continue
+        ls = l.split(',')
+        key = ls[1].upper().replace('"','')
+        # print letmap[key]
+        vals = map(fil, ls[2:])
+        # print vals
+        props[letmap[key]] =  vals
+
+# print props.keys()
+
+# for l in props.keys():
+#     print l, len(props[l])
+# sys.exit(0)
+
+
+def colname(i,j):
+    return "Pos%d_Prop%03d," % (i, j)
+
+with open(outputfile, 'w') as fmat:
+    fmat.write('PEPTIDE,response,')
+    for i in range(9):
+        for j in range(544):
+            fmat.write(colname(i+1,j+1))
+    fmat.write('\n')    
+    for pep in peps:
+        fmat.write(pep)
+        fmat.write(',')
+        # fmat.write(peps[pep])
+        # fmat.write(',')
+        for ami in pep:
+            for val in props[ami]:
+                fmat.write(str(val))
+                fmat.write(',')
+        fmat.write('\n')
+        #break
+
+
